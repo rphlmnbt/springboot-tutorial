5e9f759edc46b572b82ec469aa5a587bd3946404
rphlmnbt <rphlmnbt@gmail.com>
2021-11-02 19:29:54 +0800

    Cleaned up unused imports.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,6 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.transaction.Transactional;
 import java.util.List;
