21d9577c9befca5ddb43e65d15273acc516c5274
rphlmnbt <rphlmnbt@gmail.com>
2021-11-02 19:26:14 +0800

    Created function for retrieving all students.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -17,4 +17,8 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
 }
