c187142a5c329c8fefb7c622b35f4f9982cf8728
rphlmnbt <rphlmnbt@gmail.com>
2021-11-02 19:33:37 +0800

    Created controller function for registering new student.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -24,4 +24,9 @@ public class StudentController {
         return studentService.getStudents();
     }
 
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
 }
