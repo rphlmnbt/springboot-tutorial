d487f6eae6ec5f3bacfa084589dd45bb87a7e1e9
rphlmnbt <rphlmnbt@gmail.com>
2021-11-02 19:31:47 +0800

    Initialized student controller.

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController (StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+}
