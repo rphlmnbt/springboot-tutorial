a39accff1454a611f30e91da2e135e6740278d1e
rphlmnbt <rphlmnbt@gmail.com>
2021-11-02 19:22:22 +0800

    Initialized student service file.

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+}
